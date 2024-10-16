184270ebc2cac4bce80d2e2085ea8dca6b2a3944
mircea <mircea.rautoiu@gmail.com>
2024-10-16 18:38:54 +0300

    Add Onboarding Pages

--- app/src/main/java/com/loc/newsapp/pesentation/onboarding/components/OnBoardingPage.kt
+++ app/src/main/java/com/loc/newsapp/pesentation/onboarding/components/OnBoardingPage.kt
@@ -1,5 +1,6 @@
 package com.loc.newsapp.pesentation.onboarding.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,10 +16,13 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import com.loc.newsapp.R
 import com.loc.newsapp.pesentation.Dimens.MediumPadding1
 import com.loc.newsapp.pesentation.Dimens.MediumPadding2
 import com.loc.newsapp.pesentation.onboarding.Page
+import com.loc.newsapp.pesentation.onboarding.pages
+import com.loc.newsapp.ui.theme.NewsAppTheme
 
 @Composable
 fun OnBoardingPage(modifier: Modifier = Modifier, page: Page) {
@@ -39,6 +43,23 @@ fun OnBoardingPage(modifier: Modifier = Modifier, page: Page) {
             style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.Bold),
             color = colorResource(id = R.color.display_small)
         )
+        Text(
+            text = page.description,
+            modifier = Modifier.padding(horizontal = MediumPadding2),
+            style = MaterialTheme.typography.bodyMedium,
+            color = colorResource(id = R.color.text_medium)
+        )
     }
 
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun OnBoardingPagePreview() {
+    NewsAppTheme {
+        OnBoardingPage(
+            page = pages[0]
+        )
+    }
 }
