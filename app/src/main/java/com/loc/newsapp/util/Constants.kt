f90d2c0654f53460177ddb163217916da2ddc8b6
mircea <mircea.rautoiu@gmail.com>
2024-10-17 19:43:47 +0300

    Paging

--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -3,4 +3,6 @@ package com.loc.newsapp.util
 object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
+
+    const val API_KEY = "932890e0695c448cac1223b9b5e7841d"
 }
